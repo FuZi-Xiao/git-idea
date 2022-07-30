10795277a1e556473e97ebf36b4914c488be256b
GuoqingWang <GuoqingWang@163.com>
2022-07-30 17:54:31 +0800

    1.0version

---
+++ src/main/java/com/wgq/TestGit.java
@@ -0,0 +1,14 @@
+package com.wgq;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 王国庆~
+ * @version 1.0
+ */
+public class TestGit {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("java");
+    }
+}
